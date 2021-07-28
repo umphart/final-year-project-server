ddb873cdcce29bac562ca3fed617404dd2064215
Umar Musa <umusa5801@gmail.com>
2021-07-28 16:02:39 +0100

    update

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ const db = mysql.createPool({
  app.use(express.json())
  app.use(express.urlencoded({extended:true}))
 
- 
+ //
+ app.use('/login', (req, res) => {
+  res.send({
+    token: 'test1234'
+  });
+});
 // get from course table
 app.get('/api/get-course', (_req, res) =>{
   const sqlSelect ="SELECT * FROM courselist";
@@ -114,8 +119,7 @@ app.get('/api/get-venue', (req, res) =>{
  })
  //------------------------------------------------------------------------
  // post to  course  table
-  app.post('/api/insert-leveloneCourse', (req, res )=>{
-    console.log("hittes");    
+  app.post('/api/insert-course', (req, res )=>{   
     const cCode = req.body.cCode;
     const cTitle =  req.body.cTitle;
     const cCore =  req.body.cCore;   
@@ -193,43 +197,84 @@ app.delete("/api/delete-venue/:id", (req, res)=>{
     }
 }) 
 });
-app.put("/api/update-course/:id", (req, res)=>{
-    let unassignedlect
-    let randomLect
-    const id = req.params.id
-    console.log(id);
-    const sqlSelect ="SELECT * FROM courselist WHERE id =?" ;
-     db.query(sqlSelect, id, (err, result)=>{
-      if (err) return res.status(500).json({err})
-      if(result[0].assigned) {
-        return res.status(200).json({msg:"course has already been assigned", assigned: 1})
+
+app.get('/api/assign-all-course', (req, res) =>{
+  const updatecourse = 'UPDATE courselist SET assigned = 1, lecturerid =?  WHERE id =?'
+  const sqlInsert = 'INSERT INTO assigned (course_id, lecturer_id) VALUES (?,?)'
+  const getlecturers = 'SELECT * FROM lecturers'
+  db.query(getlecturers, (err, lecturers)=>{
+    if (err) return res.status(400).json({err})
+    const sqlSelect ="SELECT * FROM courselist";
+    db.query(sqlSelect, (err, courses)=>{
+      if (err) return res.status(400).json({err})
+      var smallerArrays = []; // will contain the sub-arrays of couses
+      if (!lecturers.length  || !courses.length) return res.status(404).json({err:'course'})
+      var arraySize = Math.floor(courses.length/lecturers.length)
+      for (var i=0;i<Math.ceil(courses.length/arraySize);i++) {
+        smallerArrays.push(courses.slice(i*arraySize,i*arraySize+arraySize));
       }
-      else {
-        const sqlSelect ="SELECT * FROM lecturers";
-        db.query(sqlSelect, (err, result)=>{
-          unassignedlect = result.filter(lecturer=>{
-            return lecturer.assigned != 1
+      //console.log(smallerArrays.length, arraySize);
+      for (var i=0; i<smallerArrays.length; i++) {
+        for(var j=0; j<smallerArrays[i].length; j++) {
+          let shadow_id
+          // lecturers.length =2
+          i >= lecturers.length ? shadow_id = (i % lecturers.length) : shadow_id = i
+       //   console.log(i,smallerArrays[i][j], lecturers[shadow_id]);
+          db.query(updatecourse,[lecturers[shadow_id]['id'],smallerArrays[i][j]['id']], (err, result)=>{
+            if (err) return res.status(400).json({err})
+            
           })
-          randomLect = unassignedlect[Math.floor(Math.random()*unassignedlect.length)]
-          const lecturerid = randomLect.id
-          const sqlUdate ="UPDATE course SET assigned = 1, lecturerid =?  WHERE id =?";
-          db.query(sqlUdate, [lecturerid,id], (err , result)=>{
-          if (err) {
-            return res.status(501).json({err})
-          }
-          const lctUdate ="UPDATE lecturer SET assigned = 1, assignedCourseId =  WHERE id =?";
-          
-          res.status(201).json({msg:"course assigned successfully", assigned:0})
-        }) 
-        })
+        }
       }
+      res.status(200).json({msg:"courses assigned to lecturers"})
     })
-})
+  })
+ })
+ //--------------------------------------------------------------//
+ app.get('/api/get-assigned-course/:id', (req, res) =>{
+  const id = req.params.id
+  //console.log({id});
+  const sqlSelect ="SELECT * FROM courselist WHERE lecturerid =?";
+  db.query(sqlSelect, id, (err, result)=>{
+    if (err) return res.status(400).json({err})
+    res.status(201).json({result})
+ })
+ })
+//--------------------------------------------------------------//
+app.get('/api/get-assigned-lect/:id', (req, res) =>{
+  const id = req.params.id
+
+  const sqlSelect ="SELECT * FROM lecturers WHERE id =?";
+  db.query(sqlSelect, id, (err, result)=>{
+    if (err) return res.status(400).json({err})
+    res.status(201).json({result})
+ })
+ })
+ app.get('/api/get-all', (req, res) =>{
+  const sqlSelect ="SELECT * FROM lecturers";
+  const getCourses ="SELECT * FROM courselist WHERE assigned =1";
+  let getAll = [];
+  db.query(sqlSelect, (err, lecturers)=>{
+    if (err) return res.status(400).json({err})
+    db.query(getCourses, (err, courses)=>{
+      lecturers.forEach(lecturer=>{
+        let info = {};
+        info.lecturer = lecturer
+        info.courses = courses.filter((course)=>{
+          if (course.lecturerid == lecturer['id']) {
+            return course
+          }
+        })
+        getAll.push(info)
+      })
+      res.status(201).json({getAll})
+    })
+  });
+ }) 
  app.get("/", (err, res)=>{
    res.send("Developing in progress")
-
  })
-
-  app.listen(9000, ()=>{
-    console.log("app..................")
+ const port = process.env.PORT ||9000
+  app.listen(port, ()=>{
+    console.log("app running on port "+port)
 });
